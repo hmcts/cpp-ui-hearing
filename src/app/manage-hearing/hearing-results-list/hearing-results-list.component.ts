4120dacdccb83f07b6f95ffa68567e8755cb837c
sunil-ari <sunil.ari@hmcts.net>
2026-08-11 12:05:44 +0100

    cleanup

--- src/app/manage-hearing/hearing-results-list/hearing-results-list.component.ts
+++ src/app/manage-hearing/hearing-results-list/hearing-results-list.component.ts
@@ -360,27 +360,6 @@ export class HearingResultsListComponent {
     defendant?.personDefendant?.bailStatus?.description ??
     REMAND_STATUS_BY_INITIATION_CODE[prosecutionCase.initiationCode] ??
     REMAND_STATUS_NOT_RECORDED;
-
-  // getRemandStatus = (
-  //   defendant: DefendantCasesApplications,
-  //   prosecutionCase: Omit<ProsecutionCaseDetails, 'defendants'>
-  // ): string => {
-  //   debugger;
-
-  //   console.log('caseURN:', prosecutionCase.prosecutionCaseIdentifier.caseURN);
-
-  //   const bailStatus = defendant?.personDefendant?.bailStatus?.description;
-  //   console.log('bailStatus:', bailStatus);
-
-  //   const remandByCode = REMAND_STATUS_BY_INITIATION_CODE[prosecutionCase.initiationCode];
-  //   console.log('remandByCode:', remandByCode);
-
-  //   const result = bailStatus ?? remandByCode ?? REMAND_STATUS_NOT_RECORDED;
-  //   console.log('final result:', result);
-
-  //   return result;
-  // };
-
   onYouthBoxSelected(defendant: DefendantCasesApplications) {
     if (!this.hasBulkCase(defendant)) {
       this.onYouthCourtToggle.emit(defendant.id);
