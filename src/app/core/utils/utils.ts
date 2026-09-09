b776748283afde35fe99c641e8a0df0816db9dee
sunil-ari <sunil.ari@hmcts.net>
2026-09-09 19:02:49 +0100

    code cleanup

--- src/app/core/utils/utils.ts
+++ src/app/core/utils/utils.ts
@@ -198,21 +198,6 @@ export const sortOffences = (
  * @returns distinct defendants
  */
 
-// export const getDistinctDefendants = (defendants: Defendant[]): Defendant[] => {
-//   const distinctDefendants: Defendant[] = [];
-//   for (const defendant of defendants) {
-//     if (
-//       !distinctDefendants.find(def => def.masterDefendantId === defendant.masterDefendantId) &&
-//       !distinctDefendants.find(def => def.id === defendant.masterDefendantId) &&
-//       !distinctDefendants.find(def => def.masterDefendantId === defendant.id)
-//     ) {
-//       distinctDefendants.push(defendant);
-//     }
-//   }
-
-//   return distinctDefendants;
-// };
-
 export const getDistinctDefendants = (defendants: Defendant[]): Defendant[] => {
   const groupedDefendants = new Map<string, Defendant>();
 
