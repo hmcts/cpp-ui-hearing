ba6ae2dcd440087614c7182730e82cf977bb3894
Shenikafa <Shenika.Fernando@HMCTS.NET>
2026-09-23 13:37:20 +0100

    CHD-2029 test snapshot update

--- jest.config.js
+++ jest.config.js
@@ -6,26 +6,27 @@ module.exports = {
   roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testRunner: 'jest-jasmine2',
+  testEnvironment: 'jsdom',
   prettierPath: '',
   testEnvironmentOptions: {
-    url: 'http://localhost/',
+    url: 'http://localhost/'
   },
   coverageThreshold: {
     global: {
       branches: 60,
       functions: 70,
       lines: 80,
-      statements: 80,
-    },
+      statements: 80
+    }
   },
   moduleNameMapper: {
     '^src/(.*)': '<rootDir>/src/$1',
     '^app/(.*)': '<rootDir>/src/app/$1',
     '^assets/(.*)': '<rootDir>/src/assets/$1',
     '^environments/(.*)': '<rootDir>/src/environments/$1',
-    '^lodash-es$': 'lodash',
+    '^lodash-es$': 'lodash'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!@ngrx|@cpp/core|ngx-bootstrap|ng2-pdf-viewer|pdfjs-dist|.*\\.mjs)',
-  ],
+    'node_modules/(?!@ngrx|@cpp/core|ngx-bootstrap|ng2-pdf-viewer|pdfjs-dist|.*\\.mjs)'
+  ]
 };
